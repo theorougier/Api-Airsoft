16e9674d83766781ca24ca1dd000a4b5e4ebebc6
JeanPilous <theo_rougier@hotmail.fr>
2021-10-18 20:47:13 +0200

    initializd

---
+++ api/config/db.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    HOST: "airsoft-bot-api.mysql.eu2.frbit.com",
+    USER: "airsoft-bot-api",
+    PASSWORD: "8Dg05Dk-W-IwmevNd4no1EIv",
+    DB: "airsoft-bot-api",
+    PORT: 3306
+  };

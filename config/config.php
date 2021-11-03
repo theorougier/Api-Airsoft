151f809d2b9a55ae4e807557f4e808fe14486cf4
JeanPilous <theo_rougier@hotmail.fr>
2021-11-03 10:16:34 +0100

    add on fortrabbit

--- config/config.php
+++ config/config.php
@@ -1,7 +1,7 @@
 <?php
 $container['db'] = function ($c) {
     $db = $c['settings']['db'];
-    $pdo = new PDO('mysql:host=' . $db['localhost'] . ';dbname=' . $db['api'],
+    $pdo = new PDO('mysql:host=' . $db['localhost'] . ';dbname=' . $db['test'],
         $db['root'], $db['']);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
